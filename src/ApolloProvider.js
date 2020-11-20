af2395e4de58f0a233c35e02a76f9a03ca6c6d24
alkotras42 <kirill.stogniy@mail.ru>
2020-11-20 21:16:36 +0300

    prep for deployment

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://sma-by-alkotras42.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
